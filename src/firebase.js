00cf3aa522a12c426a96bef4dc2b05465f42362e
Andrew <chuntcdj@gmail.com>
2020-10-05 12:27:41 +0800

    Developing WhatsApp clone, uploading a basic layout

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_ZPGEVelcjxPwVa-HRMqg2KKCtXBZRac",
+  authDomain: "whatsapp-b41b1.firebaseapp.com",
+  databaseURL: "https://whatsapp-b41b1.firebaseio.com",
+  projectId: "whatsapp-b41b1",
+  storageBucket: "whatsapp-b41b1.appspot.com",
+  messagingSenderId: "495887620196",
+  appId: "1:495887620196:web:1969aa00cabfcd3888a771",
+  measurementId: "G-R91R86Q4LL",
+};
